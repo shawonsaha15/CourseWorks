f263a7942efdce627cf85220f03a9b0a9b8437a6
Swapnil Saha Shawon <85196971+shawonsaha15@users.noreply.github.com>
2024-06-23 23:47:32 +0600

    Target and predictor variables

--- Cognizant Artificial Intelligence/AI_Module.py
+++ Cognizant Artificial Intelligence/AI_Module.py
@@ -24,3 +24,28 @@ def load_data(path: str = "/path_to_csv"):
   df = pd.read_csv(f"{path}")
   df = df.drop(columns=["Umnamed: 0"], inplace=True, errors='ignore')
   return df
+
+# Create target variable and predictor variables
+def create_target_and_predictors(
+    data: pd.DataFrame = None,
+    target: str = "estimated_stock_pct"
+):
+  """
+  This function takes in Panda DataFrame and splits it into a target column
+  and a set of predictor variables, i.e. X & y.
+  These two splits of the data will be used to train a supervised machine
+  learning model.
+
+  :param data: pd.DataFrame, dataframe containing data for the model
+  :param target: str, target variable that will be needed to predict
+  :return X: pd.DataFrame
+          y: pd.Series
+  """
+  # Check to see if target is present in the data
+  if target not in data.columns:
+    raise Exception(f"Target: {target} is not present in the data")
+
+  # Define the variables for target variable and prediction variables
+  X = data.drop(columns=[target])
+  y = data[target]
+  return X, y
